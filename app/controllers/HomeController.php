ea22d490ead3ce9c6b4af10bbd458a32d4356e87
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-03-23 11:13:16 +0300

    Validation Trait

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -6,6 +6,38 @@ use Config\Controller;
 
 class HomeController extends Controller
 {
+
+    public function valid()
+    {
+        // Required
+        $data = "  ";
+        $msg = $this->required($data);
+
+        if (empty($msg)) echo "done";
+        else echo $msg;
+
+        // isEmail
+//        $data = "ahlam@gmailcom";
+//        $msg = $this->isEmail($data);
+//
+//        if (empty($msg)) echo "done";
+//        else echo $msg;
+
+        //Max Length
+//        $data = "ahlddd";
+//        $msg = $this->max($data,3);
+//
+//        if (empty($msg)) echo "done";
+//        else echo $msg;
+
+        //Min Length
+//        $data = "ad";
+//        $msg = $this->min($data,3);
+//
+//        if (empty($msg)) echo "done";
+//        else echo $msg;
+    }
+
     public function index()
     {
         $data = ['a'=>'ahlam'];
